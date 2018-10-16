74736fd5afcb18edcb06d06ee36a0c053f69a7d6
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2018-10-16 22:43:02 +1100

    Move bokeh app files for better organization of repo

--- microscopium/js/download_data.js
+++ microscopium/js/download_data.js
@@ -1,27 +1,27 @@
-function to_tsv(data_table) {
-    var columns = Object.keys(data_table)
-    var nrows = data_table[columns[0]].length
-    var lines = [columns.join('\t')];
+var data = source.data;
+var filetext = 'info,url,x,y\n';
+for (var i = 0; i < data['info'].length; i++) {
+    var currRow = [data['info'][i].toString(),
+                   data['url'][i].toString(),
+                   data['x'][i].toString(),
+                   data['y'][i].toString().concat('\n')];
 
-    for (var i = 0; i < nrows; i++) {
-        var row = [];
-        for (var j = 0; j < columns.length; j++) {
-            var column = columns[j];
-            row.push(data_table[column][i].toString());
-        }
-        lines.push(row.join('\t'));
-    }
-    filetext = lines.join('\n').concat('\n');
-    return filetext
+    var joined = currRow.join();
+    filetext = filetext.concat(joined);
 }
 
+var filename = 'data_result.csv';
+var blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' });
 
-var filename = 'data_result.tsv';
-var filetext = to_tsv(source.data)
-var blob = new Blob([filetext], { type: 'text/tsv;charset=utf-8;' });
-var link = document.createElement("a");
-link.href = URL.createObjectURL(blob);
-link.download = filename
-link.target = "_blank";
-link.style.visibility = 'hidden';
-link.dispatchEvent(new MouseEvent('click'))
+//addresses IE
+if (navigator.msSaveBlob) {
+    navigator.msSaveBlob(blob, filename);
+} else {
+    var link = document.createElement("a");
+    link = document.createElement('a')
+    link.href = URL.createObjectURL(blob);
+    link.download = filename
+    link.target = "_blank";
+    link.style.visibility = 'hidden';
+    link.dispatchEvent(new MouseEvent('click'))
+}
